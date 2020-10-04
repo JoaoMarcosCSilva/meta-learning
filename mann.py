34e4d31517e6e0cba11498b18ade2fab9b8c6dbe
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-10-04 16:00:39 -0300

    MANN model now recieves layers as an input

--- mann.py
+++ mann.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.keras.layers import *
 
-def MANN(num_classes = 5, num_samples_per_class = 2):
+def MANN(num_classes = 5, num_samples_per_class = 2, layers = [LSTM(128, return_sequences = True), LSTM(128, return_sequences = True)]):
     images = Input(shape = (num_samples_per_class, num_classes, 28, 28, 1))
     labels = Input(shape = (num_samples_per_class, num_classes, num_classes))
 
@@ -14,12 +14,9 @@ def MANN(num_classes = 5, num_samples_per_class = 2):
     lbl = tf.reshape(lbl, (-1, num_samples_per_class * num_classes, num_classes))
 
     x = tf.concat([im, lbl], -1)
-
-#    x = Bidirectional(LSTM(512, return_sequences = True))(x)
-#    x = Bidirectional(LSTM(512, return_sequences = True))(x)
-    x = LSTM(128, return_sequences = True)(x)
-    x = LSTM(128, return_sequences = True)(x)
-    x = Dense(512, activation = 'relu')(x)
+    
+    for l in layers:
+        x = l(x)
     
     x = Dense(num_classes)(x)
     x = Softmax()(x)
